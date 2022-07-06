128515dee1df8513d540529efcb7d3eb333cb74c
Mauro Alori <mauroalori@hotmail.com>
2022-07-06 16:34:26 -0300

    se crea la funcion para ordenar una lista generada por la funcion generadora de listas y se hace el muestreo de las edades de la persona mas joven y la mas vieja en la misma funcion

--- Simple/simple.py
+++ Simple/simple.py
@@ -1,5 +1,20 @@
 import random 
 
+#cantidad de personas de la lista
+cant_personas=10
+
+#funcion para generar la lista
 def generar_lista():
-    lista=[{"id":i,"edad":random.randint(0,100)} for i in range (10)]
+    lista=[{"id":i,"edad":random.randint(0,100)} for i in range (cant_personas)]
+    return lista
+
+#funcion para ordenar la lista
+def ordenar_lista(lista=generar_lista()):
+    for i in range(cant_personas-1):
+        for j in range(cant_personas-i-1):
+            if lista[j]['edad']<lista[j+1]['edad']:
+                aux=lista[j]
+                lista[j]=lista[j+1]
+                lista[j+1]=aux
+    print(f"Edad de la persona mas joven {lista[cant_personas-1]['edad']} \nEdad de la persona mas vieja: {lista[0]['edad']}")
     return lista
