f1211a089a35b9f7a397b8e95d9ad89ba9aeb8fb
Mauro Alori <mauroalori@hotmail.com>
2022-07-07 14:22:21 -0300

    se modifican las funciones para que retornen un float redondeado a dos decimales, se realiza una validacion en la lectura del radio y se hace un muestreo del perimetro y del area del circulo con el radio ingresado

--- Clases/clases.py
+++ Clases/clases.py
@@ -6,14 +6,29 @@ class circulo:
             self.radio=rad
         else:
             print("No es posible tener un radio menor o igual a 0")
-
+    
     #funcion para calcular el área
-    def calcular_area():
+    def calcular_area(self):
         area=math.pi*pow(self.radio,2)
-        return area
+        return round(area,2)
     
     #funcion para calcular el perímetro
-    def calcular_perimetro():
+    def calcular_perimetro(self):
         perimetro=2*math.pi*self.radio
-        return perimetro
+        return round(perimetro,2)
 
+
+radio=float(input("Ingrese el radio del circulo(en cm): "))
+
+flag=True
+while(flag):
+    if(radio>0):
+        Circulo = circulo(radio)
+        flag=False
+    else:
+        print("El radio debe ser mayor a 0, por favor ingreselo nuevamente")
+        radio=float(input("Ingrese el radio del circulo(en cm): "))
+
+print(f"Perimetro: {Circulo.calcular_perimetro()}cm")
+
+print(f"Area: {Circulo.calcular_area()}cm²")
