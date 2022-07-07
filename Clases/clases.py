eafe3c1490f4f1b43ec2c2fb93cf8c92838a135c
Mauro Alori <mauroalori@hotmail.com>
2022-07-07 14:56:52 -0300

    se modifica el muestreo del objeto

--- Clases/clases.py
+++ Clases/clases.py
@@ -3,19 +3,22 @@ import math
 class circulo:
     def __init__(self,rad):
         if rad>0:
-            self.radio=rad
+            self.radio=round(rad,2)
         else:
             print("No es posible tener un radio menor o igual a 0")
     
     #funcion para calcular el área
     def calcular_area(self):
-        area=math.pi*pow(self.radio,2)
-        return round(area,2)
+        area=round(math.pi*pow(self.radio,2),2)
+        return area
     
     #funcion para calcular el perímetro
     def calcular_perimetro(self):
-        perimetro=2*math.pi*self.radio
-        return round(perimetro,2)
+        perimetro=round(2*math.pi*self.radio,2)
+        return perimetro
+    
+    def __str__(self):
+        return "El radio vale %.2fcm. El perimetro vale %.2fcm. El area vale %fcm²."% (self.radio,self.calcular_perimetro(),self.calcular_area())
 
 
 radio=float(input("Ingrese el radio del circulo(en cm): "))
@@ -29,6 +32,4 @@ while(flag):
         print("El radio debe ser mayor a 0, por favor ingreselo nuevamente")
         radio=float(input("Ingrese el radio del circulo(en cm): "))
 
-print(f"Perimetro: {Circulo.calcular_perimetro()}cm")
-
-print(f"Area: {Circulo.calcular_area()}cm²")
+print(Circulo)
